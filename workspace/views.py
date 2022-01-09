8e8321a869c489d28fef1c9ec2a6104189fc647e
kevin m <kivertyr@gmail.com>
2022-01-09 03:15:39 +0100

    færdiggør hjemmeskærm

--- workspace/views.py
+++ workspace/views.py
@@ -1,8 +1,13 @@
 from flask import Flask
-from flask.blueprints import Blueprint
+from flask import Blueprint, render_template
+
 
 views = Blueprint(__name__, "views")
 
 @views.route("/")
 def home():
-    return "home"
+    return render_template("index.html")
+
+@views.route("/hvaderkatte")
+def hvaderkatte():
+    return render_template("hvaderkatte.html")
